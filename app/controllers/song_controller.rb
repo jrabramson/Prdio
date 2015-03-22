695f3c95c7c4eeeac141d5444004bc8294ed692d
Jesse Abramson <jr@jesseabramson.com>
2015-03-22 16:17:13 -0400

    removed old rdio gem

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,5 +1,14 @@
 class SongController < ApplicationController
 	def index
-		@test = Rails.configuration.rdio[:key]
+		session.clear
+		  # begin the authentication process
+		  rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]])
+		  callback_url = (URI.join request.url, '/callback').to_s
+		  url = rdio.begin_authentication(callback_url)
+		  # save our request token in the session
+		  session[:rt] = rdio.token[0]
+		  session[:rts] = rdio.token[1]
+		  # go to Rdio to authenticate the app
+		  redirect_to url
 	end
 end
