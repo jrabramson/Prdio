7a147679065a20d8adaefca58c789ae8e727c289
Jesse Abramson <jr@jesseabramson.com>
2015-03-23 18:25:06 -0400

    finished initial API deployment

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,14 +1,63 @@
 class SongController < ApplicationController
 	def index
-		session.clear
-		  # begin the authentication process
-		  rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]])
-		  callback_url = (URI.join request.url, '/callback').to_s
-		  url = rdio.begin_authentication(callback_url)
-		  # save our request token in the session
-		  session[:rt] = rdio.token[0]
-		  session[:rts] = rdio.token[1]
-		  # go to Rdio to authenticate the app
-		  redirect_to url
+		access_token = session[:at]
+	  	access_token_secret = session[:ats]
+	  	if access_token and access_token_secret
+		  	rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]], 
+	                    [access_token, access_token_secret])
+			currentUser = rdio.call('currentUser')['result']
+			playlists = rdio.call('getPlaylists')['result']['owned']
+
+			response = "
+			<html><head><title>Rdio-Simple Example</title></head><body>
+			<p>%s's playlists:</p>
+			<ul>
+			"
+		    playlists.each do |playlist|
+		      response += '<li><a href="%s">%s</a></li>' % [playlist['shortUrl'], playlist['name']]
+		    end
+		    response += '</ul><a href="/logout">Log out of Rdio</a></body></html>'
+		    return response
+		else
+			session.clear
+		  	# begin the authentication process
+			rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]])
+			callback_url = (URI.join request.url, '/callback').to_s
+			url = rdio.begin_authentication(callback_url)
+			# save our request token in the session
+			session[:rt] = rdio.token[0]
+			session[:rts] = rdio.token[1]
+			# go to Rdio to authenticate the app
+			redirect_to url
+		end
+	end
+
+	def callback
+		# get the state from cookies and the query string
+		request_token = session[:rt]
+		request_token_secret = session[:rts]
+		verifier = params[:oauth_verifier]
+		# make sure we have everything we need
+		if request_token and request_token_secret and verifier
+			# exchange the verifier and request token for an access token
+			rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]], 
+			[request_token, request_token_secret])
+			rdio.complete_authentication(verifier)
+			# save the access token in cookies (and discard the request token)
+			session[:at] = rdio.token[0]
+			session[:ats] = rdio.token[1]
+			session.delete(:rt)
+			session.delete(:rts)
+			# go to the home page
+			redirect_to('/')
+		else
+			# we're missing something important
+			redirect_to('/logout')
+		end
+	end
+
+	def logout
+  		session.clear
+  		redirect to('/')
 	end
 end
