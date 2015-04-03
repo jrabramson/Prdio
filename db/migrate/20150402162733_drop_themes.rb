9cb3c1f94f14e2e5fc0c93576bded14ffa2f215b
Jesse Abramson <jr@jesseabramson.com>
2015-04-02 20:54:07 -0400

    song model changes

--- db/migrate/20150402162733_drop_themes.rb
+++
@@ -1,9 +0,0 @@
-class DropThemes < ActiveRecord::Migration
-  def change
-  	drop_table :themes
-  	drop_table :posts
-  	drop_table :customposts
-  	drop_table :sign_ups
-  	drop_table :users
-  end
-end
