020856c2ad5ab6221c071346548a874acb2c07e7
Scott Leitch <s.d.leitch@gmail.com>
2015-04-16 23:00:50 -0400

    added link_to seaerch on host view

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -5,24 +5,12 @@ class SongController < ApplicationController
 	end
 
 	def create
-<<<<<<< HEAD
-		access_token = session[:at]
-		access_token_secret = session[:ats]
-		@song = Song.new(song_search)
-  		@songParams = @song.genSong access_token, access_token_secret
-  		session[:song_key] = @songParams['key']
-  		@currentUser = session['user']
-  		@song.artist =  @songParams['artist']
-  		@song.key =  @songParams['key']
-		if @song.save
-=======
 		@host = Host.find_by_room params[:host_id]
 		rdio = rdio_init
   		@songParams = rdio.call('search', ({ "query" => params[:song][:title], "types" => "Track" }))['result']['results'][0]
   		@song = Song.new(title: song_search['title'], artist: @songParams['artist'], key: @songParams['key'], playlist_id: @host.playlist.id )
 		if @song.save
 			rdio.call('addToPlaylist', ({ playlist: @host.playlist.key, tracks: @song.key }))
->>>>>>> 96007dc82d71af6b9f99f0a652b95b481138d8ad
 			redirect_to '/' + @host.room
 		end
 	end
