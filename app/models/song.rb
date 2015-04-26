855b6f37567001bf3f1e8e0eeb0f3d084b705928
Scott Leitch <s.d.leitch@gmail.com>
2015-04-26 17:34:01 -0400

    index on master: 4e01907 ajax stuff

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,18 @@ class Song < ActiveRecord::Base
 	has_many :voted_songs
 	has_many :guests, through: :voted_songs
 
+	def reorder_playlist
+		rdio = rdio_init
+		order = []
+		
+		rdio.call('setPlaylistOrder', self.playlist.key, order )
+	end
+
+	def rdio_init
+		access_token = session[:at]
+	  	access_token_secret = session[:ats]
+		rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]], 
+			[access_token, access_token_secret])
+	end
+
 end
