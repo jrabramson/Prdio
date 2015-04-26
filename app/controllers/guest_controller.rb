667c409ccb4eb09047c20bcbaf1af0cfb73148ed
Scott Leitch <s.d.leitch@gmail.com>
2015-04-26 16:45:29 -0400

    removed dumb email from dumb quests

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -3,7 +3,6 @@ class GuestController < ApplicationController
 	def new 
 		@guest = Guest.new({ 
 				name: guest_params[:name], 
-				email: guest_params[:email], 
 				host_id: Host.find_by_room(guest_params[:host_id]).id
 			})
 		if @guest.save
@@ -13,7 +12,7 @@ class GuestController < ApplicationController
 	end
 
 	def guest_params 
-		guest_params = params.require(:guest).permit(:host_id, :email, :name) 
+		guest_params = params.require(:guest).permit(:host_id, :name) 
 	end
 
 end
