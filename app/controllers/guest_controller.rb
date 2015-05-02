389c1bd6279d98e63e6a3729f09b0ca77513dedb
Scott Leitch <s.d.leitch@gmail.com>
2015-05-02 18:12:43 -0400

    fixing upcase stuff

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -1,23 +1,25 @@
 class GuestController < ApplicationController
 
-	def new 
-		if Host.find_by_room(guest_params[:host_id].upcase)
+	def new
+		guest_room = guest_params[:host_id].upcase
+		guest_name = guest_params[:name]
+		if Host.find_by_room(guest_room)
 			@guest = Guest.new({ 
-					name: guest_params[:name], 
-					host_id: Host.find_by_room(guest_params[:host_id]).id
+					name: guest_name, 
+					host_id: Host.find_by_room(guest_room).id
 				})
 			if @guest.save
 				session.clear
 				session[:guest_id] = @guest.id
-				redirect_to '/party/' + params[:guest][:host_id]
+				redirect_to '/party/' + guest_room
 			end
 		else
 			redirect_to '/'
 		end
 	end
 
-	def guest_params 
-		guest_params = params.require(:guest).permit(:host_id, :name) 
+	def guest_params
+		guest_params = params.require(:guest).permit(:host_id, :name)
 	end
 
 end
