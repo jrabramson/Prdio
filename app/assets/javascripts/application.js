922cb9f7f210f56869c2a043b8b1152216600ace
Jesse Abramson <jr@jesseabramson.com>
2015-05-03 23:30:56 -0400

    cleaning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,10 +35,13 @@ $(function() {
 	  setTimeout(function(){ $('.error').fadeOut() }, 2500);
 	});
 
-	// var myScroll = new IScroll('#switcher', {
-	//     mouseWheel: true,
-	//     scrollbars: false
-	// });
+	var myScroll;
+
+	function loaded () {
+		myScroll = new IScroll('#switcher', { mouseWheel: true });
+	}
+
+	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
 	$('#switcher').css('padding-top', $('.songList').css('height'));	
 	$('#switcher').css('padding-bottom', $('.playback-container').css('height'));
