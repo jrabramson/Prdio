87ad47162fc942d7421e2efc56434cc6a3d757f0
Jesse Abramson <jr@jesseabramson.com>
2015-05-10 01:52:57 -0400

    socket

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -12,7 +12,6 @@ class GuestController < ApplicationController
 				session.clear
 				session[:guest_id] = @guest.id
 				redirect_to '/party/' + guest_room
-				raise 'hell'
 			else
 				flash[:error] = "Please enter your name!"
 				redirect_to '/'
