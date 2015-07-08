369a86a23f344c7be19975e055b76db7c13c6238
Jesse Abramson <jesse@influitive.com>
2015-07-07 21:29:08 -0400

    cleanup

--- app/models/host.rb
+++ app/models/host.rb
@@ -27,7 +27,7 @@ class Host < ActiveRecord::Base
 		access_token = host.at
 	  	access_token_secret = host.ats
 		rdio = Rdio.new([Rails.configuration.rdio[:key], Rails.configuration.rdio[:secret]], 
-			[access_token, access_token_secret])
+			access_token)
 	end
 
 	def self.party
